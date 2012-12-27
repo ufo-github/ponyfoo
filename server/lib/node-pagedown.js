52af8aab84b0c41bc610aead31b1b3605f45dfd3
bevacqua <nicolasbevacqua@gmail.com>
2012-12-26 21:47:10 -0300

    reconfigured server

--- server/lib/node-pagedown.js
+++
@@ -1,2 +0,0 @@
-exports.Converter = require("../../static/js/lib/Markdown.Converter").Converter;
-exports.getSanitizingConverter = require("../../static/js/lib/Markdown.Sanitizer").getSanitizingConverter;
