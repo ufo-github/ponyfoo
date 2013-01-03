7b521b5ba1c639e4a3280e312f01179d17e1928e
Nico Bevacqua <nicolasbevacqua@gmail.com>
2013-01-03 17:50:57 -0300

    simplified api routes

--- src/routing/core.js
+++ src/routing/core.js
@@ -4,21 +4,17 @@ var entry = require('../controllers/entry.js');
 function registerApiRoutes(server){
 	var api = '/api/1.0';
 	
-	function register(verb, endpoint, action) {
-		server[verb](api + endpoint, action);
-	}
-	
 	function put(endpoint, action) {
-		register('put',endpoint,action);
+		server.put(endpoint,action);
 	}
 	
 	function get(endpoint, action) {
-		register('get',endpoint,action);
+		server.get(endpoint,action);
 	}
 	
     put('/entry', entry.put);	
 	get('/entry/latest', entry.get);
-};
+}
 
 module.exports = function(server){	
 	registerApiRoutes(server);
