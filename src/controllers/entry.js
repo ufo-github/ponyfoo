ce2c562fce3c79c0c33ff126bd946834f53c5400
Nico Bevacqua <nicolasbevacqua@gmail.com>
2013-01-07 12:13:21 -0300

    DRYer rest api

--- src/controllers/entry.js
+++ src/controllers/entry.js
@@ -1,28 +1,46 @@
 var mongoose = require('mongoose'),
     models = require('../models/all.js'),
-	model = models.entry;
+	model = models.entry,
+	resHandler = function(err, success){
+		res.writeHead(200, { 'Content-Type': 'application/json' });
+		
+		var test = !!err;		
+		if (test){
+			var json = JSON.stringify({
+				error: true
+			});
+			res.write(json);
+			res.end();
+		}else{
+			(success || res.end)();
+		}
+	},
+	done = function(err){
+		resHandler(err);
+	};
 	
 module.exports = {
     get: function(req,res){
         var callback = function(err,documents){
+			resHandler(err, function(){
                 var json = JSON.stringify({
                     entries: documents
                 });
-                res.writeHead(200, { 'Content-Type': 'application/json' });
                 res.end(json);
-            };
+            });
+		};
 
         model.find({}).sort('-date').limit(8).exec(callback);
     },
 	
 	getOne: function(req,res){
-        var callback = function(err,document){
-                var json = JSON.stringify(document);
-				
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(json);
-            },
-			id = req.params.id;
+        var id = req.params.id,
+			callback = function(err,document){
+                resHandler(err, function(){
+					var json = JSON.stringify(document);
+					res.end(json);
+				});
+			};
 
         model.findOne({ _id: id }).exec(callback);
 	},
@@ -32,17 +50,12 @@ module.exports = {
 			instance;
 
         instance = new model(document);
-		instance.save(function(err){
-			res.end();
-		});
+		instance.save(done);
     },
 	
 	upd: function(req,res){
 		var id = req.params.id,
-            document = req.body.entry,
-            done = function(err){
-                res.end();
-            };
+            document = req.body.entry;
 		
         document.updated = new Date();
         model.findOneAndUpdate({ _id: id }, document, {}, done);
@@ -51,8 +64,6 @@ module.exports = {
 	del: function(req,res){
 		var id = req.params.id;
 			
-		model.remove({ _id: id }, function(err){
-			res.end();
-		});
+		model.remove({ _id: id }, done);
 	}
 };
