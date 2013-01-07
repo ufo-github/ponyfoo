50f42198d660e2bed918bf23510d3732c5a16f3c
Nico Bevacqua <nicolasbevacqua@gmail.com>
2013-01-07 14:27:24 -0300

    removed trim from md fields

--- src/models/entry.js
+++ src/models/entry.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose'),
     schema = new mongoose.Schema({
         title: { type: String, require: true, trim: true },
-        brief: { type: String, require: true, trim: true },
-        text: { type: String, require: true, trim: true },
+        brief: { type: String, require: true },
+        text: { type: String, require: true },
         date: { type: Date, index: { unique: true }, require: true },
 		updated: { type: Date, require: true, default: Date.now }
     });
