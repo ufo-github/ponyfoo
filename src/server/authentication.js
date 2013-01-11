21cf167aa48d58ced38431f26bd98ba898c27825
Nico Bevacqua <nicolasbevacqua@gmail.com>
2013-01-11 16:55:44 -0300

    user authentication

---
+++ src/server/authentication.js
@@ -0,0 +1,37 @@
+var passport = require('passport'),
+    models = require('../models/all.js'),
+    LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy({
+        usernameField: 'email'
+    },
+    function(email, password, done) {
+        models.user.findOne({ email: email }, function (err, user) {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                return done(null, false);
+            }
+            if (!user.validatePassword(password)) {
+                return done(null, false);
+            }
+            return done(null, user);
+        });
+    }
+));
+
+passport.serializeUser(function(user, done) {
+    done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+    models.user.findOne({ _id: id }, function (err, user) {
+        if(err){
+            return done(err);
+        }
+        return done(null, user);
+    });
+});
+
+module.exports = listen;
