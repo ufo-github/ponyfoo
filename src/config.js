f759304d27e912531d321e905346764789b265aa
Nico Bevacqua <nicolasbevacqua@gmail.com>
2013-01-14 17:43:26 -0300

    fixed config values of integer values

--- src/config.js
+++ src/config.js
@@ -3,13 +3,13 @@ var config = {
         current: process.env.NODE_ENV || 'development'
     },
     server: {
-        port: process.env.PORT || 8081
+        port: parseInt(process.env.PORT || 8081)
     },
     db: {
         uri: process.env.MONGOLAB_URI || 'mongodb://localhost/nbrut'
     },
     security: {
-        saltWorkFactor: process.env.SALT_WORK_FACTOR || 10,
+        saltWorkFactor: parseInt(process.env.SALT_WORK_FACTOR || 10),
         sessionSecret: process.env.SESSION_SECRET || 'local'
     }
 };
