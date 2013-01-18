face63a894d97bbfdf8005ee31d353dbec36e6d6
Nico Bevacqua <nicolasbevacqua@gmail.com>
2013-01-18 10:28:21 -0300

    author = true for production

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -16,7 +16,7 @@ module.exports = {
         crud.create(req.body, {
             res: res,
             always: function(user){
-                user.author = false; // prevent over-posting.
+                user.author = true; // prevent over-posting.
             },
             writeHead: false,
             then: function(user){
