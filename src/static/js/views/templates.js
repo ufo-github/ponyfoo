8d304616b11c06ac208faf8291b37363946ce97d
bevacqua <nicolasbevacqua@gmail.com>
2013-01-23 20:56:39 -0300

    fixed title in single entry

--- src/static/js/views/templates.js
+++ src/static/js/views/templates.js
@@ -46,8 +46,8 @@
         },{
             key: 'one',
             title: {
-                dynamic: function(viewModel){
-                    return viewModel.title;
+                dynamic: function(viewModel){ // always exactly a single entry.
+                    return viewModel.entries[0].title;
                 }
             },
             route: getEntryRoute(/^\/([0-9]{4})\/(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/([a-z0-9\-]+)$/i)
