b57b5c2cfd1b26b6155b610eac0307f64b3bd62b
Nico Bevacqua <nicolasbevacqua@gmail.com>
2013-01-30 17:20:46 -0300

    updated assetify

--- src/assets.js
+++ src/assets.js
@@ -3,7 +3,8 @@ var config = require('./config.js'),
         source: config.static.folder,
         bin: config.static.bin,
         css: getCss(),
-        js: getJs()
+        js: getJs(),
+        profiles: ['anon','registered','author']
     };
 
 function getCss(){
