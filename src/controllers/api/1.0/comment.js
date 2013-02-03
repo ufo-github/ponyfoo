727df0022b240af1666bd83d56b6fe99defce839
bevacqua <nicolasbevacqua@gmail.com>
2013-02-03 03:30:56 -0300

    comment api get

--- src/controllers/api/1.0/comment.js
+++ src/controllers/api/1.0/comment.js
@@ -1,26 +1,25 @@
-var async = require('async'),
-    rest = require('../../../services/rest.js'),
+var rest = require('../../../services/rest.js'),
     discussion = require('../../../models/discussion.js'),
     comment = require('../../../models/comment.js');
 
 function discuss(req,res){
     var entry = req.params.entryId,
-        model = new discussion({ entry: mongoose.Types.ObjectId(entry) });
+        document = new discussion({ entry: mongoose.Types.ObjectId(entry) });
 
-    add(req,res,model);
+    add(req,res,document);
 }
 
 function insert(req,res){
-    discussion.findOne({ _id: req.params.id }, function(err, discussion){
+    discussion.findOne({ _id: req.params.id }, function(err, document){
         if(err){
             throw err;
         }
-        add(req,res,discussion);
+        add(req,res,document);
     });
 }
 
-function add(req,res,model){
-    var document = {
+function add(req,res,document){
+    var model = {
         text: req.body.comment,
         author: {
             id: mongoose.Types.ObjectId(req.user._id),
@@ -28,16 +27,29 @@ function add(req,res,model){
         }
     };
 
-    model.comments.push(document);
-    model.save(function(err){
+    document.comments.push(model);
+    document.save(function(err){
         rest.resHandler(err,{
             res: res
         });
     });
 }
 
+function list(req,res){
+    discussion.query({ entry: mongoose.Types.ObjectId(req.params.id) }, callback);
+
+    function callback(err,documents){
+        rest.resHandler(err, {
+            res: res,
+            then: function(){
+                rest.end(res,documents);
+            }
+        });
+    }
+}
+
 module.exports = {
-    get: null,
+    get: list,
     discuss: discuss,
     ins: insert
 };
