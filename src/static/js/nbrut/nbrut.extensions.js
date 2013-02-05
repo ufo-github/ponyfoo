11c459c6c5c93b894ec897e67754093cfe526f9e
Nico Bevacqua <nicolasbevacqua@gmail.com>
2013-02-05 11:31:06 -0300

    removed debug msg

--- src/static/js/nbrut/nbrut.extensions.js
+++ src/static/js/nbrut/nbrut.extensions.js
@@ -13,7 +13,6 @@
 	};
 
     $.fn.anchorSEO = function(){
-        console.log(this.selector);
         return this.each(function(){
             var a = $(this).addClass('highlighted-link'),
                 url = a.get(0).href;
