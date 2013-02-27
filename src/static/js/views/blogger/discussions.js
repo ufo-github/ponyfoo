7b86b1f55ba8560921f5d8c700fdd8ae9d9b3786
bevacqua <nicolasbevacqua@gmail.com>
2013-02-27 00:57:06 -0300

    yup, that wasnt needed

--- src/static/js/views/blogger/discussions.js
+++ src/static/js/views/blogger/discussions.js
@@ -10,24 +10,16 @@
 
 	function afterActivate(viewModel, data, ctx){
         var table = ctx.elements.filter('.discussion-table');
-console.log(viewModel);
+
         nbrut.ui.pagedTable({
             what: 'discussion',
             friendlyName: 'discussions',
             legend: 'Load older discussions',
             partial: 'discussion-rows',
             paging: viewModel.paging,
-            table: table,
-            then: prettify
+            table: table
         });
-
-        prettify(table);
 	}
-
-    function prettify(container){
-        var code = container.find('.comment');
-        nbrut.md.prettify(code);
-    }
 
     nbrut.tt.configure({
         key: 'blogger-discussions',
