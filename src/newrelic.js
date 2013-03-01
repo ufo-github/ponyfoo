5aeb81877bcd2ec9fe47664ba53396495a8ffc98
Nico Bevacqua <nicolasbevacqua@gmail.com>
2013-03-01 12:31:34 -0300

    new relic config

--- src/newrelic.js
+++ src/newrelic.js
@@ -1,15 +1,11 @@
 var config = require('./config.js');
 
 function init(){
-    if(!!process.env.NEW_RELIC_LICENSE_KEY){
+    if(!!process.env.NEW_RELIC_NO_CONFIG_FILE && !!process.env.NEW_RELIC_LICENSE_KEY){
         require('newrelic');
     }
 }
 
 module.exports = {
-    init: init,
-    app_name : [config.site.title],
-    logging : {
-        level : 'info'
-    }
+    init: init
 };
