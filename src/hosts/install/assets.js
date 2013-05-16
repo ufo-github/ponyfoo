f895cc75a69a064db4e8f2fc2780502c2cabbbf5
bevacqua <nicolasbevacqua@gmail.com>
2013-05-16 18:12:50 -0300

    moved some less around

--- src/hosts/install/assets.js
+++ src/hosts/install/assets.js
@@ -1,8 +1,13 @@
 'use strict';
 
-var configuration = require('../common/assets.js').configure(__dirname);
+var configuration = require('../common/assets.js').configure(__dirname),
+    path = require('path'),
+    absolute = path.join(process.cwd(), '/src/frontend/less'),
+    shared = path.relative(configuration.assets.source, absolute);
 
-configuration.assets.css = [/*
+configuration.assets.css = [
+    shared + '/defaults/reset.css',/*
+    '/css/defaults/basic.less',
     '/css/defaults/elements.less',
     '/css/defaults/controls.less',
     '/css/defaults/controls.spinner.less',
