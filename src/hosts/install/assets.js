65cf74738ef0c069bf4b0f59f693ca949e43359e
bevacqua <nicolasbevacqua@gmail.com>
2013-05-17 01:10:36 -0300

    all css assets for install

--- src/hosts/install/assets.js
+++ src/hosts/install/assets.js
@@ -6,15 +6,15 @@ var them = require('../common/assets.js').configure(__dirname),
     shared = path.relative(them.assets.source, absolute);
 
 them.assets.css = [
-    shared + '/defaults/reset.css',
-    shared + '/css/defaults/basic.less',
-    shared + '/css/defaults/elements.less',
-    shared + '/css/defaults/controls.less',
-    shared + '/css/defaults/controls.spinner.less',
-    shared + '/css/defaults/layout.less',
-    shared + '/css/defaults/design.less',
-    shared + '/css/defaults/sprite.less',
-    shared + '/css/vendor/markdown.less',
+    path.join(shared, '/defaults/reset.css'),
+    path.join(shared, '/defaults/basic.less'),
+    path.join(shared, '/defaults/elements.less'),
+    path.join(shared, '/defaults/controls.less'),
+    path.join(shared, '/defaults/controls.spinner.less'),
+    path.join(shared, '/defaults/layout.less'),
+    path.join(shared, '/defaults/design.less'),
+    path.join(shared, '/defaults/sprite.less'),
+    path.join(shared, '/vendor/markdown.less'),
     '/css/views/home/index.less'
 ];
 
