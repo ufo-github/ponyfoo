4e8266a96e499c95c09a23c9e8a7f6a89520cd5b
bevacqua <nicolasbevacqua@gmail.com>
2013-07-21 23:50:19 -0300

    updated assetify config

--- src/hosts/common/assetDataBuilder.js
+++ src/hosts/common/assetDataBuilder.js
@@ -23,6 +23,8 @@ module.exports = {
                 jsn: true,
                 forward: [{ extnames: ['.eot', '.svg', '.ttf', '.woff'] }, true]
             },
+            compress: true,
+            fingerprint: true,
             expires: /^img/i,
             resolveFrontendPath: function(file){
                 return path.join(relative, file);
