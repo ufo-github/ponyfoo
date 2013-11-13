43531c48a1d048f5d7900dd0458186ce1ccb62cc
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-11-13 12:46:10 -0200

    Update Gruntfile.js

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -6,12 +6,7 @@ module.exports = function(grunt) {
     var opts = require('./config');
 
     grunt.initConfig(opts);
-
-    require('load-npm-tasks')(grunt);
-
-    if(config.env.development){
-        grunt.loadNpmTasks('grunt-contrib-watch');
-    }
-
     grunt.loadTasks('./build/tasks');
+    
+    require('load-npm-tasks')(grunt);
 };
