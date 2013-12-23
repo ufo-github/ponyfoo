bcdc2cc995994d1e6aa534b831a164ead76a6143
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-12-23 13:57:50 -0300

    pingback support removed

--- src/hosts/blog/controllers/pingbackController.js
+++ src/hosts/blog/controllers/pingbackController.js
@@ -4,7 +4,15 @@ var Pingback = require('pingback'),
     pingbackService = require('../../../service/pingbackService.js');
 
 module.exports = {
+    receive: function (req, res, next) {
+        console.log('Pingback unsupported at the moment.');
+        next();
+    }
+};
+/*
+module.exports = {
     receive: Pingback.middleware(function(source, target, next){
         pingbackService.handle(this.req.blog, this, next);
     })
 };
+*/
