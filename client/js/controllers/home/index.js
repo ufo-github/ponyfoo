5e9987e39766709ae5bf5cbe61d7a96ce82553ba
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-05-13 21:17:27 -0300

    foo

---
+++ client/js/controllers/home/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = function (model) {
+  console.log('home');
+};
--- client/js/routing.js
+++ client/js/routing.js
@@ -3,6 +3,10 @@
 // this file should be auto-generated.
 
 module.exports = [{
+  route: '/',
+  template: require('../../.bin/views/home/index'),
+  controller: require('./controllers/home/index')
+}, {
   route: '/author/compose',
   template: require('../../.bin/views/author/compose'),
   controller: require('./controllers/author/compose')
