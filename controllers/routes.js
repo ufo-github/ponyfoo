da0640b2abd6af15ef2facdbfc1f38c6f709fdef
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-05-14 15:26:32 -0300

    accomodate to taunus

--- controllers/routes.js
+++ controllers/routes.js
@@ -6,16 +6,3 @@ module.exports = [{
   route: '/', action: 'home/index',
   route: '/author/compose', action: 'author/compose', middleware: author.only
 }];
-
-
-// binary to compile into:
-/*module.exports = [{
-  route: '/',
-  template: require('../../.bin/views/home/index'),
-  controller: require('./controllers/home/index')
-}, {
-  route: '/author/compose',
-  template: require('../../.bin/views/author/compose'),
-  controller: require('./controllers/author/compose')
-},];
-*/
--- controllers/routing.js
+++ controllers/routing.js
@@ -4,28 +4,10 @@ var taunus = require('taunus');
 var routes = require('./routes');
 var article = require('./article');
 var errors = require('../lib/errors');
-var defaults = {
-  description: '',
-  author: {
-    meta: 'Nicolas Bevacqua <foo@bevacqua.io>',
-    twitter: '@nzgb'
-  },
-  images: {
-    cover: '',
-    list: []
-  },
-  partial: 'error/not-found',
-  model: {
-    title: 'Pony Foo'
-  }
-};
 
 module.exports = function (app) {
   app.get('/api/articles', article.list);
 
-  taunus.mount(app, routes, {
-    defaults: defaults,
-    views: '.bin/views'
-  });
+  taunus.mount(app, routes);
   app.use(errors.handler);
 };
