675d92604f5f4cf42fd9c54f4e0174c575c4d1de
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-05-15 00:11:55 -0300

    flexarea component sort of there

--- client/js/controllers/author/compose.js
+++ client/js/controllers/author/compose.js
@@ -1,6 +1,9 @@
 'use strict';
 
+var flexarea = require('./flexarea'); // require('flexarea');
+
 module.exports = function () {
-  var ta = document.querySelectorAll('.ac-textarea');
   console.log('compose controller');
+  var ta = document.querySelectorAll('.ac-textarea');
+  Array.prototype.forEach.call(ta, flexarea);
 };
--- client/js/controllers/author/flexarea.js
+++ client/js/controllers/author/flexarea.js
@@ -1,61 +1,58 @@
-
-  var textarea, staticOffset;
-  var iLastMousePos = 0;
-  var iMin = 32;
-  var grip;
-
-  function startDrag (e) {
-    textarea = $(e.data.el);
-    textarea.blur();
-    iLastMousePos = mousePosition(e).y;
-    staticOffset = textarea.height() - iLastMousePos;
-    textarea.addClass('resizing');
-    $(document).mousemove(performDrag).mouseup(endDrag);
-    return false
-  }
-
-  function performDrag (e) {
-    var iThisMousePos = mousePosition(e).y;
-    var iMousePos = staticOffset + iThisMousePos;
-    if (iLastMousePos >= (iThisMousePos)) {
-      iMousePos -= 5
-    }
-    iLastMousePos = iThisMousePos;
-    iMousePos = Math.max(iMin, iMousePos);
-    textarea.height(iMousePos + 'px');
-    if (iMousePos < iMin) {
-      endDrag(e)
-    }
-    return false
-  }
-
-  function endDrag (e) {
-    $(document).unbind('mousemove', performDrag).unbind('mouseup', endDrag);
-    textarea.removeClass('resizing');
-    textarea.focus();
-    textarea = null;
-    staticOffset = null;
-    iLastMousePos = 0
-  }
-
-  function mousePosition (e) {
-    return {
-      x: e.clientX + document.documentElement.scrollLeft,
-      y: e.clientY + document.documentElement.scrollTop
-    }
-  }
-})(jQuery);
+'use strict';
 
 module.exports = function (textarea) {
   var wrapper = document.createElement('div');
   var grip = document.createElement('div');
+  var min = 32;
+  var offset;
+  var position;
 
-  textarea.classList.add('fl-textarea');
+  textarea.classList.add('fa-textarea');
   textarea.parentNode.replaceChild(wrapper, textarea);
   wrapper.appendChild(textarea);
   wrapper.appendChild(grip);
-  wrapper.classList.add('fl-wrapper');
-  grip.classList.add('fl-grip');
-  grip.addEventListener('mousedown', startDrag);
+  wrapper.classList.add('fa-wrapper');
+  grip.classList.add('fa-grip');
+  grip.addEventListener('mousedown', start);
   grip.style.marginRight = (grip.offsetWidth - textarea.offsetWidth) + 'px';
+
+
+  function start (e) {
+    textarea.blur();
+    textarea.classList.add('fa-textarea-resizing');
+    position = getPosition(e).y;
+    offset = textarea.style.height - position;
+    document.addEventListener('mousemove', move);
+    document.addEventListener('mouseup', end);
+    return false;
+  }
+
+  function move (e) {
+    var current = getPosition(e).y;
+    var moved = offset + current;
+    if (position >= current) {
+      moved -= 5;
+    }
+    position = current;
+    moved = Math.max(min, moved);
+    textarea.style.height = moved + 'px';
+    if (moved < min) {
+      end(e);
+    }
+    return false;
+  }
+
+  function end (e) {
+    document.removeEventListener('mousemove', move);
+    document.removeEventListener('mouseup', end);
+    textarea.classList.remove('fa-textarea-resizing');
+    textarea.focus();
+  }
+
+  function getPosition (e) {
+    return {
+      x: e.clientX + document.documentElement.scrollLeft,
+      y: e.clientY + document.documentElement.scrollTop
+    };
+  }
 };
