26772f9ee2f321d3527a90830b7ac78ac9712d1e
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-05-17 03:01:06 -0300

    rm console log

--- client/js/controllers/author/compose.js
+++ client/js/controllers/author/compose.js
@@ -3,7 +3,6 @@
 var flexarea = require('flexarea');
 
 module.exports = function () {
-  console.log('compose controller');
   var ta = document.querySelectorAll('.ac-textarea');
   Array.prototype.forEach.call(ta, flexarea);
 };
