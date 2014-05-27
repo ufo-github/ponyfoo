1ac352e97b6d010729965a75579c178d318c9dae
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-05-26 21:54:00 -0300

    refactored blog svc

--- services/blog.js
+++ services/blog.js
@@ -1,75 +1,69 @@
 'use strict';
 
-var Blog = require('../model/Blog.js'),
-    User = require('../model/User.js'),
-    config = require('../config');
+var Blog = require('../models/Blog');
+var User = require('../models/User');
 
-function create(owner, slug, title, done){
-    new Blog({
-        owner: owner._id,
-        slug: slug,
-        title: title,
-        social: {
-            rss: true
-        }
-    }).save(done);
-}
-
-function validate(model,done){
-    var email = model['user.email'],
-        password = model['user.password'],
-        title = model['blog.title'];
-
-    if(!email || !password){ // the most basic form of validation, since it's just the super admin
-        return done('Oops, you should fill out every field');
+function create (owner, slug, title, done) {
+  new Blog({
+    owner: owner._id,
+    slug: slug,
+    title: title,
+    social: {
+      rss: true
     }
-    if(!title){
-        return done('Oops, you forgot to pick a blog title!');
+  }).save(done);
+}
+
+function validate (model,done) {
+  var email = model['user.email'],
+    password = model['user.password'],
+    title = model['blog.title'];
+
+  if(!email || !password) { // the most basic form of validation, since it's just the super admin
+    return done('Oops, you should fill out every field');
+  }
+  if(!title) {
+    done('Oops, you forgot to pick a blog title!'); return;
+  }
+  done(null, {
+    email: email,
+    password: password,
+    title: title
+  });
+}
+
+function findBySlug (slug, done) {
+  Blog.findOne({ slug: slug }).lean().exec(foundBlog);
+
+  function foundBlog (err, blog) {
+    if (err || !blog) {
+      done(err); return;
     }
-    done(null, {
-        email: email,
-        password: password,
-        title: title
-    });
-}
 
-function findBySlug(slug, done){
-    var query = { slug: slug };
+    User.findOne({ _id: blog.owner }).lean().exec(foundUser);
 
-    if(!config.server.slug.enabled){
-        delete query.slug;
+    function foundUser (err, user) {
+      if (err || !user) {
+        done(err); return;
+      }
+
+      done(err, { blog: blog, blogger: user });
     }
-
-    Blog.findOne(query).lean().exec(function(err, blog){
-        if(err || !blog){
-            return done(err, blog);
-        }
-
-        User.findOne({ _id: blog.owner }).lean().exec(function(err, user){
-            if(err || !user){
-                return done(err);
-            }
-
-            done(err, {
-                blog: blog,
-                blogger: user
-            });
-        });
-    });
+  }
 }
 
-function findByUser(user, done){
-    Blog.findOne({ owner: user._id }, done);
+function findByUser (user, done) {
+  Blog.findOne({ owner: user._id }, done);
 }
 
-function findById(id, done){
-    Blog.findOne({ _id: id }, done);
+function findById (id, done) {
+  Blog.findOne({ _id: id }, done);
 }
 
 module.exports = {
-    create: create,
-    validate: validate,
-    findById: findById,
-    findBySlug: findBySlug,
-    findByUser: findByUser
+  create: create,
+  validate: validate,
+  findById: findById,
+  findBySlug: findBySlug,
+  findByUser: findByUser
 };
