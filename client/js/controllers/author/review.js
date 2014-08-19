4060c22ca33f64cf8125de0d2e25d9fb700e93e8
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-08-19 11:15:06 -0300

    sprites and delegate delete

--- client/js/controllers/author/review.js
+++ client/js/controllers/author/review.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = function (viewModel) {
+  var container = $('.ar-container');
 
+  container.on('click', '.ic-remove', remove);
+
+  function remove () {
+    viewModel.measly.delete('/api/articles/' + article._id);
+  }
 };
