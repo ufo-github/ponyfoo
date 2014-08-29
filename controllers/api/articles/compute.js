dc6b551b8df8323fb6b774e8ae4298cf68c59d83
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-08-29 11:36:26 +0200

    moved stuff around

---
+++ controllers/api/articles/compute.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var articleSearch = require('../../../services/articleSearch');
+
+module.exports = function (req, res, next) {
+  articleSearch.refreshRelated(respond);
+
+  function respond (err) {
+    if (err) {
+      next(err); return;
+    }
+    res.json(200, {});
+  }
+};
