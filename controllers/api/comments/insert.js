ed46bee49533cfd850e8c22af6e7a711f84ab7b6
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-09-06 19:29:28 -0300

    somewhere

--- controllers/api/comments/insert.js
+++ controllers/api/comments/insert.js
@@ -2,12 +2,14 @@
 
 var _ = require('lodash');
 var contra = require('contra');
+var winston = require('winston');
 var Article = require('../../../models/Article');
 var Comment = require('../../../models/Comment');
 var User = require('../../../models/User');
 var validate = require('./lib/validate');
 var respond = require('../lib/respond');
 var markdownFatService = require('../../../services/markdownFat');
+var htmlService = require('../../../services/html');
 var subscriberService = require('../../../services/subscriber');
 
 module.exports = function (req, res, next) {
@@ -56,32 +58,51 @@ module.exports = function (req, res, next) {
     }
   }
 
-  function getRecipients (article) {
-    var emails = [article.author.email];
-    var thread, op;
-    var parentId = model.parent;
-    if (parentId) {
-      op = article.comments.id(parentId);
-      thread = article.comments.filter(sameThread);
-      thread.unshift(op);
-      emails = emails.concat(_.pluck(thread, 'email'));
-    }
-    function sameThread (comment) {
-      return comment.parent && comment.parent.equals(parentId);
-    }
-    return _(emails).uniq().without(model.email).value();
+  function notify (article) {
+    contra.concurrent({
+      recipients: contra.curry(findRecipients, article),
+      html: absolutizeHtml
+    }, send);
   }
 
-  function notify (article) {
-    contra.waterfall([
-      function (next) {
-        article.populate('author', next);
-      },
-      function (article, next) {
-        var emails = getRecipients(article);
-        console.log(emails);
+  function findRecipients (article, next) {
+    contra.waterfall([hydrate, calculate], next);
+
+    function hydrate (next) {
+      article.populate('author', next);
+    }
+
+    function calculate (article, next) {
+      var emails = [article.author.email];
+      var thread, op;
+      var parentId = model.parent;
+      if (parentId) {
+        op = article.comments.id(parentId);
+        thread = article.comments.filter(sameThread);
+        thread.unshift(op);
+        emails = emails.concat(_.pluck(thread, 'email'));
       }
-    ]);
+      function sameThread (comment) {
+        return comment.parent && comment.parent.equals(parentId);
+      }
+      next(null, _(emails).uniq().without(model.email).value());
+    }
+  }
+
+  function absolutizeHtml (next) {
+    htmlService.absolutize(model.contentHtml, next);
+  }
+TODO: populate model.
+  function send (err, data) {
+    if (err) {
+      winston.info('An error occurred when preparing comment email notifications', err);
+      return;
+    }
+    var model = {
+      to: data.recipients,
+      comment: data.html
+    };
+    emailService.send('comment-published', model, emailService.logger);
   }
 
   function response (err) {
