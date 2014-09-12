a71cd5ac1afe74574737eafb54215f3e2b5379f7
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-09-12 00:37:55 -0300

    updates

--- controllers/defaultRequestModel.js
+++ controllers/defaultRequestModel.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var pkg = require('../package.json');
 var env = require('../lib/env');
 var name = env('NODE_ENV');
 var authority = env('AUTHORITY');
@@ -7,6 +8,9 @@ var authority = env('AUTHORITY');
 function defaultRequestModel (req, done) {
   done(null, {
     model: {
+      pkg: {
+        version: pkg.version
+      },
       env: {
         name: name,
         authority: authority
