0fa8b6c157c0eaf052b4f9f8f52a09773516301a
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-09-15 17:40:56 -0300

    Pre-deploy commit because :police_car:

--- cluster.js
+++ cluster.js
@@ -4,17 +4,21 @@ process.chdir(__dirname);
 
 var fs = require('fs');
 var recluster = require('recluster');
-var options = {
-  workers: 2, backoff: 10
-};
+var options = { workers: 2, backoff: 10 };
 var cluster = recluster('app.js', options);
 
-fs.writeFileSync('.pid', process.pid.toString());
+fs.writeFileSync('.pid', process.pid.toString() + '\n');
+
 cluster.run();
 
 process.on('SIGUSR2', function() {
   console.log('Cluster got SIGUSR2, reloading...');
   cluster.reload();
+});
+
+process.on('exit', function () {
+  console.log('Cluster shutting down...');
+  fs.unlinkSync('.pid');
 });
 
 console.log('Cluster executing with pid: ' + process.pid);
