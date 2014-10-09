d9c76d165fa167d5a2da88f5956776642863f73b
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-10-09 18:47:24 +0200

    Pre-deploy commit because :musical_score:

--- client/js/main.js
+++ client/js/main.js
@@ -1,34 +1,34 @@
-// 'use strict';
+'use strict';
 
-// var $ = require('dominus');
-// var ponymark = require('ponymark');
-// var taunus = require('taunus');
-// var moment = require('moment');
-// var markdownService = require('../../services/markdown');
-// var conventions = require('./conventions');
-// var analytics = require('./analytics');
+var $ = require('dominus');
+var ponymark = require('ponymark');
+var taunus = require('taunus');
+var moment = require('moment');
+var markdownService = require('../../services/markdown');
+var conventions = require('./conventions');
+var analytics = require('./analytics');
 
-// global.$ = $;
-// global.moment = moment;
-// global.taunus = taunus;
-// global.md = markdownService.compile;
+global.$ = $;
+global.moment = moment;
+global.taunus = taunus;
+global.md = markdownService.compile;
 
-// var wiring = require('./wiring');
-// var main = $.findOne('.ly-main');
+var wiring = require('./wiring');
+var main = $.findOne('.ly-main');
 
-// require('hint');
+require('hint');
 
-// taunus.on('start', function (container, viewModel) {
-//   require('./search');
-//   require('./subscriptions');
-//   analytics(viewModel.env);
-// });
+taunus.on('start', function (container, viewModel) {
+  require('./search');
+  require('./subscriptions');
+  analytics(viewModel.env);
+});
 
-// ponymark.configure({
-//   markdown: markdownService.compile,
-//   imageUploads: '/api/markdown/images'
-// });
+ponymark.configure({
+  markdown: markdownService.compile,
+  imageUploads: '/api/markdown/images'
+});
 
-// conventions();
+conventions();
 
-// taunus.mount(main, wiring);
+taunus.mount(main, wiring);
