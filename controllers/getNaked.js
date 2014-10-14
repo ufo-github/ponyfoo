b5026650bcf9bb047759f6a905714d0c9b0b9035
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-10-14 05:54:50 +0200

    bump taunus for fix

--- controllers/getNaked.js
+++ controllers/getNaked.js
@@ -2,7 +2,7 @@
 
 module.exports = function (req, res, next) {
   if (req.hostname === 'blog.ponyfoo.com') {
-    res.redirect('http://ponyfoo.com/' + req.url);
+    res.redirect('http://ponyfoo.com/' + req.url, 301);
   } else {
     next();
   }
