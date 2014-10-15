11db86d72b2fa84bd9dd40f572a41cc5814fa080
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-10-16 01:45:19 +0200

    bump imgur use by ponymark

--- controllers/api/markdown/images.js
+++ controllers/api/markdown/images.js
@@ -5,7 +5,25 @@ var ponymark = require('ponymark');
 var env = require('../../../lib/env');
 var dir = path.resolve('./temp/images');
 
-module.exports = ponymark.images({
-  imgur: env.IMGUR_API_KEY,
-  local: dir
-});
+module.exports = function (req, res, next) {
+  var image = req.files && req.files.image;
+  var options = {
+    production: env('NODE_ENV') === 'production',
+    imgur: env('IMGUR_API_KEY'),
+    local: dir,
+    image: image
+  };
+
+  ponymark.imageUpload(options, uploaded);
+
+  function uploaded (err, result) {
+    if (err) {
+      errored(err.message); return;
+    }
+    res.status(200).json(result);
+  }
+
+  function errored (message) {
+    res.status(400).json({ messages: [message] });
+  }
+};
