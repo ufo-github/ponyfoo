4deae0f586f9f9e30febef949d7ef1ad3b143ad2
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-10-16 20:49:00 -0300

    keep drafts atop of article review view

--- controllers/author/review.js
+++ controllers/author/review.js
@@ -1,29 +1,34 @@
 'use strict';
 
+var _ = require('lodash');
 var moment = require('moment');
 var Article = require('../../models/Article');
 var articleService = require('../../services/article');
 var longDate = 'dddd Do, MMMM YYYY [at] HH:mm';
 
-module.exports = function (req, res, next) {
+module.exports = getModel;
+
+function getModel (req, res, next) {
   Article.find({}).sort('-created').exec(respond);
 
   function respond (err, articles) {
     if (err) {
       next(err); return;
     }
+    var hydrated = articles.map(articleService.toJSON).map(hydrate);
+    var sorted = _.sortBy(hydrated, sortByStatus);
     res.viewModel = {
       model: {
         title: 'Article Review',
         meta: {
           canonical: '/author/review'
         },
-        articles: articles.map(articleService.toJSON).map(hydrate)
+        articles: sorted
       }
     };
     next();
   }
-};
+}
 
 function hydrate (article) {
   article._ = {};
@@ -41,4 +46,8 @@ function hydrate (article) {
     article._.conditionLabel = 'Slated for publication on ' + when.format(longDate);
   }
   return article;
+}
+
+function sortByStatus (article) {
+  return { draft: 0, publish: 1, published: 2 }[article.status];
 }
