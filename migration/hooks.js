dc81edcc18fb711a5f5f00c3023b827081d51727
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-10-16 04:34:04 +0200

    hooks enforcer

---
+++ migration/hooks.js
@@ -0,0 +1,48 @@
+'use strict';
+
+require('../lib/logging').configure();
+
+var _ = require('lodash');
+var contra = require('contra');
+var winston = require('winston');
+var db = require('../lib/db');
+var models = require('../models');
+var env = require('../lib/env');
+var markupService = require('../services/markup');
+var m;
+
+env('BULK_INSERT', true);
+db(operational);
+
+function operational () {
+  winston.info('Connected to target database!');
+  m = models();
+  ready();
+}
+
+function ready () {
+  m.Article.find({}).populate('comments').exec(resave);
+
+  function resave (err, articles) {
+    if (err) {
+      throw err;
+    }
+    contra.each(articles, save, done);
+
+    function save (article, next) {
+      article.comments.forEach(saveComment);
+      article.save(next);
+    }
+
+    function saveComment (comment) {
+      comment.contentHtml = markupService.compile(comment.content);
+    }
+  }
+
+  function done (err) {
+    if (err) {
+      throw err;
+    }
+    process.exit();
+  }
+}
