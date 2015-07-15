9910f28d4f4a2765a5edb7c686065d8777719490
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2015-07-15 12:36:37 -0300

    plea to pay server bills

--- client/js/main.js
+++ client/js/main.js
@@ -12,17 +12,37 @@ var g = global;
 
 require('hint');
 
-taunus.on('start', function (container, viewModel) {
-  require('./search');
-  require('./subscriptions');
-  analytics(viewModel.env);
-  require('./welcome')(viewModel);
-});
-
 conventions();
 
+taunus.on('start', starting);
 taunus.mount(main, wiring, { bootstrap: 'manual' });
 
 g.$ = $;
 g.md = markdownService.compile;
 g.moment = moment;
+
+setTimeout(helpMePay, 7000);
+
+function starting (container, viewModel) {
+  require('./search');
+  require('./subscriptions');
+  analytics(viewModel.env);
+  require('./welcome')(viewModel);
+  setTimeout(refreshesAd, 0);
+}
+
+function helpMePay () {
+  var ad = $('#carbonads').length !== 0;
+  if (ad === false) {
+    $('.ca-help-me').removeClass('uv-hidden');
+    taunus.off('change', refreshAd);
+  }
+}
+
+function refreshesAd () {
+  taunus.on('change', refreshAd);
+}
+
+function refreshAd () {
+  g._carbonads.refresh();
+}
