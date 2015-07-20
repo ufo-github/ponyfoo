642e8ef3d77ff727e26692a1e8348d217e6b8e6a
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2015-07-20 18:25:20 -0300

    allow codepen.io iframes and scripts

--- services/markdown.js
+++ services/markdown.js
@@ -1,12 +1,13 @@
 'use strict';
 
 var megamark = require('megamark');
+var insane = require('insane');
 var textService = require('./text');
 var domains = [
+  '//codepen.io/',
+  '//assets.codepen.io/',
   'http://codepen.io/',
-  'http://jsbin.com/',
-  'http://jsfiddle.net/',
-  'http://embed.plnkr.co/'
+  'http://assets.codepen.io/'
 ];
 
 megamark.parser.renderer.rules.link_open = link;
@@ -31,12 +32,22 @@ function link (tokens, i) {
 }
 
 function filter (token) {
-  return token.tag !== 'iframe' || domains.some(starts);
+  console.log(token)
+  var unsourced = token.tag !== 'iframe' && token.tag !== 'script';
+  return unsourced || domains.some(starts);
   function starts (beginning) {
     return token.attrs.src.indexOf(beginning) === 0;
   }
 }
 
 function compile (text) {
-  return megamark(text, { filter: filter });
+  return megamark(text, {
+    sanitizer: {
+      filter: filter,
+      allowedTags: insane.defaults.allowedTags.concat('iframe', 'script'),
+      allowedAttributes: {
+        script: ['src', 'async']
+      }
+    }
+  });
 }
