7aec7bf316c85bed19e449d5fe96fb5c99132a74
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2015-08-02 23:18:22 -0300

    cluster logs uncaught

--- cluster.js
+++ cluster.js
@@ -3,10 +3,13 @@
 require('./preconfigure');
 require('./chdir');
 
+var winston = require('winston');
 var lipstick = require('lipstick');
 var cores = require('os').cpus().length;
 var workers = Math.max(cores, 2);
 var env = require('./lib/env');
+var db = require('./lib/db');
+var logging = require('./lib/logging');
 var port = env('PORT');
 
 if (module.parent) {
@@ -20,4 +23,14 @@ function start () {
     port: port,
     workers: workers
   });
+  logging.configure();
+  db(operational);
+}
+
+function operational () {
+  process.on('uncaughtException', fatal);
+}
+
+function fatal (err) {
+  winston.error('Uncaught exception (cluster)', { error: err, stack: err.stack || '(none)' }, exit);
 }
