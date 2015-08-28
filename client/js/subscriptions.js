dedc926ccf6763cc9872a703baad3061a5df4c5e
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2015-08-28 17:24:38 -0300

    subscribe with source

--- client/js/subscriptions.js
+++ client/js/subscriptions.js
@@ -5,6 +5,7 @@ var measly = require('measly');
 
 function bind (container) {
   var ajax = measly.layer({ context: container[0] });
+  var source = $('.ss-source', container).value();
   var input = $('.ss-input', container);
   var button = $('.ss-button', container);
   button.on('click', search);
@@ -14,7 +15,7 @@ function bind (container) {
     var email = input.value().trim();
     if (email) {
       ajax.put('/api/subscribers', {
-        json: { subscriber: email }
+        json: { subscriber: email, source: source }
       });
     }
   }
