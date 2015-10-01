eb0aebc993fb9a9dd5cac74d614c3e986780fdc4
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2015-10-01 15:20:22 -0300

    proper subscription failure messaging

--- controllers/api/subscribers/lib/create.js
+++ controllers/api/subscribers/lib/create.js
@@ -10,11 +10,17 @@ function create (email, source, done) {
 
   subscriberService.add({ email: email, source: source || 'intent' }, result);
 
-  function result (err) {
+  function result (err, success, existed) {
     if (err) {
       done(err); return;
     }
-    done(null, 200, ['Thanks! You should be promptly getting an email with activation instructions.']);
+    if (success) {
+      done(null, 200, ['Thanks! You should be promptly getting an email with activation instructions.']);
+    } else if (existed) {
+      done(null, 200, ['Looks like you are already subscribed!']);
+    } else {
+      done(null, 500, ['Uh, oh! Something went wrong!']);
+    }
   }
 }
 
