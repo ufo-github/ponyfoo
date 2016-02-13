326045600a83f1f054af6b47530aa73fba6733cd
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2016-02-13 16:30:26 -0300

    options to show/hide page views and subscribers in report

--- client/js/controllers/lib/subscribers.js
+++ client/js/controllers/lib/subscribers.js
@@ -22,11 +22,15 @@ module.exports = function (viewModel, container) {
 
     render();
     $(window).on('resize', renderTimely);
+    $('#as-show-subscribers').on('change', render);
+    $('#as-show-pageviews').on('change', render);
   }
 
   function render () {
     var d3 = global.d3;
     var parent = $.findOne('.as-container', container);
+    var showSubscribers = $('#as-show-subscribers', container).value();
+    var showPageViews = $('#as-show-pageviews', container).value();
 
     $('.as-chart', container).remove();
 
@@ -38,31 +42,6 @@ module.exports = function (viewModel, container) {
     var unboundHeight = Math.ceil(dx / 1.5) - margin.top - margin.bottom;
     var width = dx - margin.left - margin.right;
     var height = Math.max(unboundHeight, 300);
-
-    var data = cloneDeep(viewModel.subscribers);
-    var x = d3.scale
-      .ordinal()
-      .rangeRoundBands([0, width], 0.1);
-
-    var y = d3.scale
-      .linear()
-      .rangeRound([height - 150, 0]);
-
-    var color = d3.scale
-      .ordinal()
-      .range(colors);
-
-    var xAxis = d3.svg
-      .axis()
-      .scale(x)
-      .orient('bottom');
-
-    var yAxis = d3.svg
-      .axis()
-      .scale(y)
-      .orient('left')
-      .tickFormat(d3.format('.2s'));
-
     var svg = d3
       .select('.as-container')
       .append('div')
@@ -73,139 +52,188 @@ module.exports = function (viewModel, container) {
       .append('g')
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-    color.domain(d3.keys(data[0]).filter(function(key) { return key !== 'date' && key !== 'dateText'; }));
+    var x = d3.scale
+      .ordinal()
+      .rangeRoundBands([0, width], 0.1);
 
-    data.forEach(function(d) {
-      var y0 = 0;
-      d.date = new Date(d.date);
-      d.fragments = color.domain().map(function(name) {
-        return { name: name, y0: y0, y1: y0 += +d[name], d: d };
-      });
-      d.total = d.unverified + d.migration + d.twitter + d.sidebar + d.comment + d.article + d.landed;
-    });
+    var y = d3.scale
+      .linear()
+      .rangeRound([height - 150, 0]);
 
-    data.sort(function(a, b) { return moment(a.date).isAfter(b.date) ? 1 : -1; });
+    var data = cloneDeep(viewModel.subscribers);
+    var color = d3.scale
+      .ordinal()
+      .range(colors);
 
-    x.domain(data.map(function(d) { return d.dateText; }));
-    y.domain([0, d3.max(data, function(d) { return d.total; })]);
+    prepareDomain();
+    addTimeAxis();
 
-    svg.append('g')
-      .attr('class', 'as-x as-axis')
-      .attr('transform', 'translate(0,' + height + ')')
-      .call(xAxis)
-      .selectAll('text')
-      .attr('y', 0)
-      .attr('x', -140)
-      .attr('dy', '.35em')
-      .attr('transform', 'rotate(70) translate(10,-45)')
-      .style('text-anchor', 'start')
-      .each(function (d, i) {
-        if (i % 2 !== 0) {
-          this.innerHTML = '';
-        }
-      });
-
-    svg.append('g')
-      .attr('class', 'as-y as-axis')
-      .call(yAxis)
-      .append('text')
-      .attr('transform', 'rotate(-90)')
-      .attr('y', 6)
-      .attr('dy', '.71em')
-      .style('text-anchor', 'end')
-      .text('Subscribers per week');
-
-    var date = svg.selectAll('.date')
-      .data(data)
-      .enter().append('g')
-      .attr('class', 'as-g')
-      .attr('transform', function(d) { return 'translate(' + x(d.dateText) + ',0)'; });
-
-    date.selectAll('.as-bar')
-      .data(function(d) { return d.fragments; })
-      .enter().append('rect')
-      .attr('class', 'as-bar')
-      .attr('width', x.rangeBand())
-      .attr('y', function(d) { return y(d.y1); })
-      .attr('height', function(d) { return y(d.y0) - y(d.y1); })
-      .style('fill', function(d) { return color(d.name); });
-
-    addLegends();
-    addTips();
-    addPageViews(x(data[0].dateText), x(data[data.length - 1].dateText) + x.rangeBand());
-
-    function addLegends () {
-      var legend = svg.selectAll('.legend')
-        .data(color.domain().slice().reverse())
-        .enter().append('g')
-        .attr('class', 'as-legend')
-        .attr('transform', function(d, i) { return 'translate(-' + (width - 160) + ',' + i * 20 + ')'; });
-
-      legend.append('rect')
-        .attr('x', width - 18)
-        .attr('width', 18)
-        .attr('height', 18)
-        .style('fill', color);
-
-      legend.append('text')
-        .attr('x', width - 24)
-        .attr('y', 9)
-        .attr('dy', '.35em')
-        .style('text-anchor', 'end')
-        .text(function(d) { return d; });
+    if (showSubscribers) {
+      addSubscribers();
+    }
+    if (showPageViews) {
+      addPageViews(x(data[0].dateText), x(data[data.length - 1].dateText) + x.rangeBand());
     }
 
-    function addTips () {
-      var tip = d3.tip()
-        .attr('class', 'as-tip')
-        .direction('e')
-        .offset([-10, 0])
-        .html(function (d) {
-          var full = d.d;
-          var i = data.indexOf(full);
-          var oldIndex = i - 1;
-          var old = oldIndex < 0 ? 0 : data[oldIndex].total - data[oldIndex].unverified;
-          var now = full.total - full.unverified;
-          var c = color(d.name);
-          return textService.format([
-            '<div class="as-tip-content" style="color: %s; background-color: %s;">',
-              '<div>',
-                '<span class="as-tip-label">%s:</span>',
-                ' %s (%s)',
-              '</div>',
-              '<div>Overall: %s (%s)</div>',
-            '</div>'
-            ].join(''),
-            c === '#ffe270' ? '#333' : '#fbf9ec',
-            c,
-            d.name,
-            full[d.name],
-            diffText(data[oldIndex][d.name], full[d.name]),
-            now,
-            diffText(old, now)
-          );
-          function diffText (old, now) {
-            var diff = old === 0 ? 100 : (now === 0 ? -100 : (now - old) / Math.abs(old) * 100);
-            var sign = diff < 0 ? '' : '+';
-            var fixed = diff.toFixed(2).replace(/0+$/, '').replace(/\.$/, '');
-            return sign + fixed + '%';
+    function prepareDomain () {
+      color.domain(d3.keys(data[0]).filter(function (key) { return key !== 'date' && key !== 'dateText'; }));
+
+      data.forEach(function (d) {
+        var y0 = 0;
+        d.date = new Date(d.date);
+        d.fragments = color.domain().map(function (name) {
+          return { name: name, y0: y0, y1: y0 += +d[name], d: d };
+        });
+        d.total = d.unverified + d.migration + d.twitter + d.sidebar + d.comment + d.article + d.landed;
+      });
+      data.sort(function (a, b) { return moment(a.date).isAfter(b.date) ? 1 : -1; });
+
+      x.domain(data.map(function (d) { return d.dateText; }));
+      y.domain([0, d3.max(data, function (d) { return d.total; })]);
+    }
+
+    function addTimeAxis () {
+      var xAxis = d3.svg
+        .axis()
+        .scale(x)
+        .orient('bottom');
+
+      svg.append('g')
+        .attr('class', 'as-x as-axis')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(xAxis)
+        .selectAll('text')
+        .attr('y', 0)
+        .attr('x', -140)
+        .attr('dy', '.35em')
+        .attr('transform', 'rotate(70) translate(10,-45)')
+        .style('text-anchor', 'start')
+        .each(function (d, i) {
+          if (i % 2 !== 0) {
+            this.innerHTML = '';
           }
         });
+    }
 
-      svg.call(tip);
-      svg
-        .selectAll('rect')
-        .on('mouseover', function (d) {
-          if (d.y1 - d.y0 > 0) { tip.show(d); }
-        });
+    function addSubscribers () {
+      addStackedBarChart();
+      addSubscribersAxis();
+      addLegends();
+      addTips();
 
-      $(parent).on('click', tip.hide);
+      function addStackedBarChart () {
+        var date = svg.selectAll('.date')
+          .data(data)
+          .enter().append('g')
+          .attr('class', 'as-g')
+          .attr('transform', function (d) { return 'translate(' + x(d.dateText) + ',0)'; });
+
+        date.selectAll('.as-bar')
+          .data(function (d) { return d.fragments; })
+          .enter().append('rect')
+          .attr('class', 'as-bar')
+          .attr('width', x.rangeBand())
+          .attr('y', function (d) { return y(d.y1); })
+          .attr('height', function (d) { return y(d.y0) - y(d.y1); })
+          .style('fill', function (d) { return color(d.name); });
+      }
+
+      function addSubscribersAxis () {
+        if (showPageViews) {
+          return;
+        }
+        var yAxis = d3.svg
+          .axis()
+          .scale(y)
+          .orient('left')
+          .tickFormat(d3.format('.2s'));
+
+        svg.append('g')
+          .attr('class', 'as-y as-axis')
+          .call(yAxis)
+          .append('text')
+          .attr('transform', 'rotate(-90)')
+          .attr('y', 6)
+          .attr('dy', '.71em')
+          .style('text-anchor', 'end')
+          .text('Subscribers per week');
+      }
+
+      function addLegends () {
+        var legend = svg.selectAll('.legend')
+          .data(color.domain().slice().reverse())
+          .enter().append('g')
+          .attr('class', 'as-legend')
+          .attr('transform', function (d, i) { return 'translate(-' + (width - 160) + ',' + i * 20 + ')'; });
+
+        legend.append('rect')
+          .attr('x', width - 18)
+          .attr('width', 18)
+          .attr('height', 18)
+          .style('fill', color);
+
+        legend.append('text')
+          .attr('x', width - 24)
+          .attr('y', 9)
+          .attr('dy', '.35em')
+          .style('text-anchor', 'end')
+          .text(function (d) { return d; });
+      }
+
+      function addTips () {
+        var tip = d3.tip()
+          .attr('class', 'as-tip')
+          .direction('e')
+          .offset([-10, 0])
+          .html(function (d) {
+            var full = d.d;
+            var i = data.indexOf(full);
+            var oldIndex = i - 1;
+            var old = oldIndex < 0 ? 0 : data[oldIndex].total - data[oldIndex].unverified;
+            var now = full.total - full.unverified;
+            var c = color(d.name);
+            return textService.format([
+              '<div class="as-tip-content" style="color: %s; background-color: %s;">',
+                '<div>',
+                  '<span class="as-tip-label">%s:</span>',
+                  ' %s (%s)',
+                '</div>',
+                '<div>Overall: %s (%s)</div>',
+              '</div>'
+              ].join(''),
+              c === '#ffe270' ? '#333' : '#fbf9ec',
+              c,
+              d.name,
+              full[d.name],
+              diffText(data[oldIndex][d.name], full[d.name]),
+              now,
+              diffText(old, now)
+            );
+            function diffText (old, now) {
+              var diff = old === 0 ? 100 : (now === 0 ? -100 : (now - old) / Math.abs(old) * 100);
+              var sign = diff < 0 ? '' : '+';
+              var fixed = diff.toFixed(2).replace(/0+$/, '').replace(/\.$/, '');
+              return sign + fixed + '%';
+            }
+          });
+
+        svg.call(tip);
+        svg
+          .selectAll('rect')
+          .on('mouseover', function (d) {
+            if (d.y1 - d.y0 > 0) { tip.show(d); }
+          });
+
+        $(parent).on('click', tip.hide);
+      }
     }
 
     function addPageViews (x1, x2) {
       var peak = 0;
       var pv = cloneDeep(viewModel.pageviews);
-
+      if (!pv || pv.length === 0) {
+        return;
+      }
       pv.forEach(function (pv) {
         pv.date = new Date(pv.date);
         if (peak < pv.views) {
@@ -217,38 +245,67 @@ module.exports = function (viewModel, container) {
       var y = d3.scale.linear().rangeRound([height - 150, 0]);
 
       var line = d3.svg.line()
-        .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.views); });
+        .x(function (d) { return x(d.date); })
+        .y(function (d) { return y(d.views); });
 
-      x.domain(d3.extent(pv, function(d) { return d.date; }));
-      y.domain(d3.extent(pv, function(d) { return d.views; }));
+      x.domain(d3.extent(pv, function (d) { return d.date; }));
+      y.domain(d3.extent(pv, function (d) { return d.views; }));
 
-      svg.append('linearGradient')
-        .attr('id', 'as-pageviews-gradient')
-        .attr('gradientUnits', 'userSpaceOnUse')
-        .attr('x1', 0).attr('y1', y(0))
-        .attr('x2', 0).attr('y2', y(peak))
-        .selectAll('stop')
-        .data([
-          { offset: '0%', color: '#55acee' },
-          { offset: '25%', color: '#900070' },
-          { offset: '100%', color: '#e92c6c' }
-        ])
-        .enter().append('stop')
-        .attr('offset', function (d) { return d.offset; })
-        .attr('stop-color', function (d) { return d.color });
+      addPageViewsAxis();
+      addLinearGradient();
+      addPageViewsShadow();
+      addPageViewsPath();
 
-      svg
-        .append('path')
-        .datum(pv)
-        .attr('class', 'as-pageviews-shadow')
-        .attr('d', line);
+      function addPageViewsAxis () {
+        var yAxis = d3.svg
+          .axis()
+          .scale(y)
+          .orient('left')
+          .tickFormat(d3.format('.2s'));
 
-      svg
-        .append('path')
-        .datum(pv)
-        .attr('class', 'as-pageviews')
-        .attr('d', line);
+        svg.append('g')
+          .attr('class', 'as-y as-axis')
+          .call(yAxis)
+          .append('text')
+          .attr('transform', 'rotate(-90)')
+          .attr('y', 6)
+          .attr('dy', '.71em')
+          .style('text-anchor', 'end')
+          .text('Page views per day');
+      }
+
+      function addLinearGradient () {
+        svg.append('linearGradient')
+          .attr('id', 'as-pageviews-gradient')
+          .attr('gradientUnits', 'userSpaceOnUse')
+          .attr('x1', 0).attr('y1', y(0))
+          .attr('x2', 0).attr('y2', y(peak))
+          .selectAll('stop')
+          .data([
+            { offset: '0%', color: '#55acee' },
+            { offset: '25%', color: '#900070' },
+            { offset: '100%', color: '#e92c6c' }
+          ])
+          .enter().append('stop')
+          .attr('offset', function (d) { return d.offset; })
+          .attr('stop-color', function (d) { return d.color });
+      }
+
+      function addPageViewsShadow () {
+        svg
+          .append('path')
+          .datum(pv)
+          .attr('class', 'as-pageviews-shadow')
+          .attr('d', line);
+      }
+
+      function addPageViewsPath () {
+        svg
+          .append('path')
+          .datum(pv)
+          .attr('class', 'as-pageviews')
+          .attr('d', line);
+      }
     }
   }
 };
