259b59a27c6231e554ee494f7aaf36fa7fd1d786
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2016-02-28 10:51:57 -0300

    design consolidation steps

--- client/js/controllers/marketing/subscribe.js
+++ client/js/controllers/marketing/subscribe.js
@@ -1,3 +1,27 @@
 'use strict';
 
-module.exports = require('../lib/subscribers');
+var $ = require('dominus');
+var taunus = require('taunus');
+var subscribers = require('../lib/subscribers');
+
+module.exports = function (viewModel, container) {
+  var topics = $('.ss-topic', container);
+  var featureList = $.findOne('.ss-features', container);
+
+  topics.on('click', changedTopics);
+  subscribers(viewModel, container);
+
+  function changedTopics () {
+    var model = {
+      topics: topics.filter(byValue).map(toTopic)
+    };
+    taunus.partial(featureList, 'marketing/subscriber-features', model);
+  }
+
+  function byValue (el) {
+    return $(el).value();
+  }
+  function toTopic (el) {
+    return $(el).text();
+  }
+}
--- controllers/marketing/subscribe.js
+++ controllers/marketing/subscribe.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var pullData = require('../lib/pullData');
+var subscriberService = require('../../services/subscriber');
 
 module.exports = function (req, res, next) {
   pullData(function render (err, result) {
@@ -11,7 +12,8 @@ module.exports = function (req, res, next) {
     res.viewModel = {
       model: {
         title: 'Subscribe to Pony Foo!',
-        subscriberGraph: result
+        subscriberGraph: result,
+        topics: subscriberService.getTopics()
       }
     };
     next();
