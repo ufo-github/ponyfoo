dcb908e034eb39f7d4c4fdada4198bae69c425f4
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2016-03-03 19:30:54 -0300

    typography

--- controllers/api/secret/twitter-leads.js
+++ controllers/api/secret/twitter-leads.js
@@ -9,53 +9,66 @@ var Subscriber = require('../../../models/Subscriber');
 var username = env('TWITTER_LEADS_USERNAME');
 var password = env('TWITTER_LEADS_PASSWORD');
 var ads = env('TWITTER_LEADS_ADS_ID');
-var card = env('TWITTER_LEADS_CARD_ID');
+var articles = env('TWITTER_LEADS_CARD_ID_ARTICLES');
+var newsletter = env('TWITTER_LEADS_CARD_ID_NEWSLETTER');
 
 function remodel (req, res, next) {
-  if (!username || !password || !ads || !card) {
+  if (!username || !password || !ads || (!articles && !newsletter)) {
     res.status(500).json({ error: 500, message: 'Method not implemented.' });
     return;
   }
-  Subscriber.findOne({ source: 'twitter' }).sort('-created').exec(function found (err, last) {
-    if (err) {
-      next(err); return;
-    }
-    var since = last ? last.created : new Date(0);
-    var options = {
-      username: username,
-      password: password,
-      ads: ads,
-      card: card,
-      since: since
-    };
-    leads(options, pulled);
-    function pulled (err, leads) {
+
+  var tasks = [];
+  if (articles) {
+    tasks.push(pull('articles', articles));
+  }
+  if (newsletter) {
+    tasks.push(pull('newsletter', newsletter));
+  }
+  contra.concurrent(tasks);
+
+  function pull (key, id) {
+    Subscriber.findOne({ source: 'twitter:articles' }).sort('-created').exec(function found (err, last) {
       if (err) {
         next(err); return;
       }
-      if (leads.length) {
-        winston.info('Found %s lead%s via Twitter Cards', leads.length, leads.length === 1 ? '' : 's');
-      } else {
-        winston.debug('No new leads via Twitter Cards');
+      var since = last ? last.created : new Date(0);
+      var options = {
+        username: username,
+        password: password,
+        ads: ads,
+        card: card,
+        since: since
+      };
+      leads(options, pulled);
+      function pulled (err, leads) {
+        if (err) {
+          next(err); return;
+        }
+        if (leads.length) {
+          winston.info('Found %s lead%s via Twitter Cards', leads.length, leads.length === 1 ? '' : 's');
+        } else {
+          winston.debug('No new leads via Twitter Cards');
+        }
+        contra.each(leads, 3, follow, done);
+        function follow (lead, next) {
+          subscriberService.add({
+            created: lead.time,
+            email: lead.email,
+            name: lead.name,
+            source: 'twitter',
+            verified: true
+          }, next);
+        }
       }
-      contra.each(leads, 3, follow, done);
-      function follow (lead, next) {
-        subscriberService.add({
-          created: lead.time,
-          email: lead.email,
-          name: lead.name,
-          source: 'twitter',
-          verified: true
-        }, next);
+      function done (err) {
+        if (err) {
+          next(err); return;
+        }
+        res.json({});
       }
-    }
-    function done (err) {
-      if (err) {
-        next(err); return;
-      }
-      res.json({});
-    }
-  });
+    });
+  }
 }
 
 module.exports = remodel;
--- services/weeklyCompiler.js
+++ services/weeklyCompiler.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var assign = require('assignment');
 var map = require('contra/map');
+var assign = require('assignment');
+var beautifyText = require('beautify-text');
 var stylus = require('./lib/stylus');
 var textService = require('./text');
 var linkSectionView = require('../.bin/views/shared/partials/weekly/link');
@@ -25,7 +26,7 @@ function compile (sections, options, done) {
     done(null, result.join(''));
   }
   function toHeaderSectionHtml (section, next) {
-    next(null, textService.format('<div class=" wy-section-header"><h%s class="md-markdown" style="color:%s;background-color:%s;padding:10px;">%s</h%s></div>',
+    next(null, textService.format('<div class="wy-section-header"><h%s class="md-markdown" style="color:%s;background-color:%s;padding:10px;">%s</h%s></div>',
       section.size,
       section.foreground,
       section.background,
@@ -40,7 +41,11 @@ function compile (sections, options, done) {
   function toLinkSectionHtml (section, next) {
     var descriptionHtml = options.markdown.compile(section.description);
     var base = { knownTags: knownTags, descriptionHtml: descriptionHtml };
-    next(null, linkSectionView(assign(base, section)));
+    var extended = assign(base, section, {
+      title: beautifyText(section.title),
+      source: beautifyText(section.source)
+    });
+    next(null, linkSectionView(extended));
   }
   function toStylesSectionHtml (section, next) {
     stylus.render(section.styles, { filename: 'inline-styles.css' }, compiled);
