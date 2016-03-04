6c4514336f62b0bda67d35ba089846899a894a15
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2016-03-04 17:12:33 -0300

    cleanup weekly scheduler

--- services/weeklyScheduler.js
+++ services/weeklyScheduler.js
@@ -1,11 +1,12 @@
 'use strict';
 
-var winston = require('winston');
 var moment = require('moment');
+var winston = require('winston');
 var correcthorse = require('correcthorse');
 var WeeklyIssue = require('../models/WeeklyIssue');
 var weeklyFeedService = require('./weeklyFeed');
 var weeklySharingService = require('./weeklySharing');
+var settingService = require('./setting');
 
 /*
  * [A] Find an issue that's released to patrons? Verify publication, update RSS feed & release to everyone else.
@@ -16,7 +17,22 @@ var weeklySharingService = require('./weeklySharing');
  * [F] Do nothing
  */
 function run (done) {
-  findReleasedToPatrons();
+  var levels = ['debug', 'info'];
+  var level = 'debug';
+  settingService.getKey('PONYFOOWEEKLY_CRON_LEVEL', got);
+  function got (err, value) {
+    if (err) {
+      started(); end(err); return;
+    }
+    if (levels.indexOf(value) !== -1) {
+      level = value;
+    }
+    started();
+    findReleasedToPatrons();
+  }
+  function started () {
+    winston[level]('Weekly scheduler job run started.');
+  }
   function findReleasedToPatrons () {
     WeeklyIssue
       .findOne({ status: 'released', statusReach: 'patrons' })
@@ -28,10 +44,12 @@ function run (done) {
       end(err); return;
     }
     if (!issue) {
-      findScheduledOrReady(); return;
+      winston[level]('No weeklies marked as "released+patrons".');
+      findScheduledOrReady();
+      return;
     }
     if (moment(issue.publication).isBefore(moment())) {
-      winston.debug('Found weekly "%s" in "patrons" status. Not ready for "everyone" yet.', issue.slug);
+      winston[level]('Found weekly "%s" in "patrons" status. Not ready for "everyone" yet.', issue.slug);
       end();
       return;
     }
@@ -48,13 +66,13 @@ function run (done) {
       end(err); return;
     }
     if (!issue) {
-      winston.debug('No weeklies marked as "ready".', issue.slug);
-      end(err);
+      winston[level]('No weeklies marked as "ready".');
+      end();
       return;
     }
     if (issue.publication) {
       if (moment(issue.publication).subtract(1, 'days').isBefore(moment())) {
-        winston.debug('Found weekly "%s" in "ready" status. Not ready for "patrons" yet!', issue.slug);
+        winston[level]('Found weekly "%s" in "ready" status. Not ready for "patrons" yet!', issue.slug);
         end();
         return;
       }
@@ -64,9 +82,8 @@ function run (done) {
     }
   }
   function attemptToSetPublicationDate (issue) {
-    var target = getPublicationTarget();
-    if (!target) {
-      winston.debug('Weekly "%s" is "ready" but it’s too soon to schedule it.', issue.slug);
+    var target = getPublicationTarget(issue);
+    if (target === null) {
       end();
       return;
     }
@@ -80,8 +97,8 @@ function run (done) {
       }
       var latest = latestIssue && latestIssue.issue || 0;
       var number = latest + 1;
-      issue.publication = target;
       issue.statusReach = 'scheduled';
+      issue.publication = target.toDate();
       issue.slug = number.toString();
       issue.issue = number;
       issue.save(saved);
@@ -89,36 +106,43 @@ function run (done) {
         if (err) {
           end(err); return;
         }
-        winston.debug('Weekly "%s" now scheduled for publication on %s.', issue.slug, moment(issue.publication).format('MMMM D, YYYY'));
+        winston[level]('Weekly "%s" now scheduled for publication on %s.', issue.slug, moment(target).format('MMMM D, YYYY'));
         foundScheduledOrReady(null, issue);
       }
     }
   }
-  function getPublicationTarget () {
+  function getPublicationTarget (issue) {
     // thursday, of the current week, at noon.
     var target = moment().day(4).startOf('day').add(12, 'hours');
     // if the target date is before the end of today, then wait until next week
-    var tooLate = target.isBefore(moment().endOf('day'));
+    var tooLate = moment().endOf('day').isAfter(target);
+    if (tooLate) {
+      winston[level]('Weekly "%s" not scheduled because target is in the past (%s).', issue.slug, target.format('MMMM D, YYYY'));
+      return null;
+    }
     // if the target date is in over three days, then wait a few more hours
-    var tooSoon = moment(target).add(3, 'days').isAfter(moment());
-    if (tooSoon || tooLate) {
-      return;
+    var tooSoon = moment().add(3, 'days').isBefore(target);
+    if (tooSoon) {
+      winston[level]('Weekly "%s" not scheduled because target is in over three days (%s).', issue.slug, target.format('MMMM D, YYYY'));
+      return null;
     }
     return target;
   }
-  function releaseToPatrons (readyIssue) {
-    readyIssue.status = 'released';
-    readyIssue.statusReach = 'patrons';
-    readyIssue.thanks = correcthorse();
-    readyIssue.save(saved);
+  function releaseToPatrons (issue) {
+    winston[level]('Weekly "%s" being released to patrons.', issue.slug);
+    issue.status = 'released';
+    issue.statusReach = 'patrons';
+    issue.thanks = correcthorse();
+    issue.save(saved);
     function saved (err) {
       if (err) {
         end(err); return;
       }
-      sendToPatrons(readyIssue);
+      sendToPatrons(issue);
     }
   }
   function releaseToEveryoneElse (issue) {
+    winston[level]('Weekly "%s" being released to everyone.', issue.slug);
     issue.statusReach = 'everyone';
     issue.save(saved);
     function saved (err) {
@@ -130,13 +154,17 @@ function run (done) {
     }
   }
   function sendToPatrons (issue) {
+    winston[level]('Weekly "%s" being emailed to patrons.', issue.slug);
     weeklySharingService.email(issue, { patrons: 'only', thanks: issue.thanks }, end);
   }
   function sendToEveryoneElse (issue) {
+    winston[level]('Weekly "%s" being emailed to everyone.', issue.slug);
     weeklySharingService.email(issue, { patrons: 'no' }, end);
   }
   function end (err) {
-    (done || log)(err);
+    log(err);
+    winston[level]('Weekly scheduler job run finished.');
+    (done || noop)(err);
   }
   function log (err) {
     if (err) {
@@ -144,6 +172,8 @@ function run (done) {
     }
   }
 }
+
+function noop () {}
 
 module.exports = {
   run: run
