7308989c0ae04afbd561942982057d72eab386ab
Nicolás Bevacqua <nicolasbevacqua@gmail.com>
2016-06-28 00:34:27 -0300

    sponsored link stats script

---
+++ scripts/count-sponsored-links.js
@@ -0,0 +1,41 @@
+'use strict';
+
+require('../preconfigure');
+require('../chdir');
+
+var moment = require('moment');
+var db = require('../lib/db');
+var boot = require('../lib/boot');
+var WeeklyIssue = require('../models/WeeklyIssue')
+
+boot(booted);
+
+function booted () {
+  WeeklyIssue.find({ statusReach: 'everyone' }).lean().exec(found);
+}
+
+function found (err, issues) {
+  if (err) {
+    throw err;
+  }
+  var sections = issues
+    .map(issue => issue.sections)
+    .reduce((all, sections) => all.concat(sections), [])
+    .filter(section => section.type === 'link')
+    .filter(section => section.sponsored);
+
+  sections.forEach(section => console.log(section.href));
+
+  console.log(`Numbers:
+     Found ${sections.length} sponsored links
+    Across ${issues.length} issues since ${moment(issues[0].created).format(`MMMM YYYY`)}
+    Earned $${sections.length * 60}.00 in total
+    Earned $${(sections.length * 60 / issues.length).toFixed(2)} per issue on average`);
+
+  end();
+}
+
+
+function end () {
+  db.disconnect();
+}
