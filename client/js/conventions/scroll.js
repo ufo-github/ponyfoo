27f57cf3a802e4862e2e2ad11cf5dec693954430
Nicolás Bevacqua <nicolasbevacqua@gmail.com>
2016-10-02 11:40:27 -0300

    scroll indicator fixes

--- client/js/conventions/scroll.js
+++ client/js/conventions/scroll.js
@@ -4,13 +4,20 @@ const $ = require(`dominus`);
 const scroll = require(`../lib/scroll`);
 
 function init () {
-  scroll.track(({ scrolled }) => {
-    const negated = 100 - scrolled;
-    const visible = scrolled > 6;
-    const show = visible ? `removeClass` : `addClass`;
-    $(`.sp-line-wrapper`)[show](`uv-hidden`);
-    $(`.sp-line`).css(`width`, negated + `%`);
+  scroll.track(({ scrolled, distances }) => {
+    if (distances.length === 1 && distances[0].element === document.body) {
+      set(); return;
+    }
+    const visibility = scrolled > 6;
+    const width = 100 - scrolled;
+    set({ visibility, width });
   });
+}
+
+function set ({ visibility = false, width = 0 } = {}) {
+  const show = visibility ? `removeClass` : `addClass`;
+  $(`.sp-line-wrapper`)[show](`uv-hidden`);
+  $(`.sp-line`).css(`width`, width + `%`);
 }
 
 module.exports = init;
